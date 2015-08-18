3352d4dcd1bc854e94f274cd7a3f6401d38d231d
Alex Welch <me@alexwelch.com>
2015-08-18 22:00:29 +0000

    total wip

--- listen.rb
+++ listen.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'json'
 require 'pp'
+require 'pry'
+require 'yaml'
 
 set :bind, '0.0.0.0'
 
@@ -15,26 +17,50 @@ post '/pr' do
   puts "action: #{res['action']}"
   puts "SHA: #{res['pull_request']['head']['sha']}"
   puts "REF: #{res['pull_request']['head']['ref']}"
-  puts "CLONE URL: #{res['pull_request']['head']['repo']['clone_url']}"
+  puts "CLONE URL: #{res['pull_request']['head']['repo']['git_url']}"
   puts "REPO NAME: #{res['pull_request']['head']['repo']['name']}"
   puts "-------------------------"
 
   action = res['action']
-  clone_url = res['pull_request']['head']['repo']['clone_url']
+  clone_url = res['pull_request']['head']['repo']['git_url']
   repo_name = res['pull_request']['head']['repo']['name']
   sha = res['pull_request']['head']['sha']
 
-  if action == 'opened' #todo what about reopened?
+  if %w(opened reopened).include?(action)
     puts 'DOIN WORK'
     `mkdir projects`
     Dir.chdir 'projects'
     `git clone #{clone_url} #{repo_name}`
     Dir.chdir repo_name
     `git checkout #{sha}`
-    `docker-machine create --driver digitalocean --digitalocean-access-token #{ENV['DO_TOKEN']} todo_allow_name_to_be_configurable`
-    puts 'DO TOKEN: ', ENV['DO_TOKEN']
-    #TODO: clean up 
-  else
-    puts 'DO NOTHING'
+    machine_name = "zazzle" # todo allow_name_to_be_configurable
+    puts 'about to provision...\n'
+    provision_machine(machine_name)
+    build_and_run
   end
+end
+
+def build_and_run
+  puts "deploying... to #{ENV['DOCKER_HOST']}"
+  `docker-compose up -d`
+  puts "done deploying...\n\n"
+end
+
+def provision_machine(machine_name)
+  if name = ENV['DOCKER_MACHINE_NAME'].to_s.empty?
+    puts "creating #{machine_name}...\n"
+    `docker-machine create --driver digitalocean --digitalocean-access-token #{ENV['DO_TOKEN']} #{machine_name}`
+  else
+    puts "using existing machine: #{ENV['DOCKER_MACHINE_NAME']}"
+  end
+  #TODO: clean up 
+  puts "machine created, setting envs...\n"
+  env_lines = `docker-machine env #{machine_name}`
+  env_lines.each_line do |line|
+    k, v = line.to_s.gsub("export ", "").strip.split("=")
+    if k && v 
+      ENV[k]=YAML.load(v)
+    end
+  end
+  binding.pry
 end
